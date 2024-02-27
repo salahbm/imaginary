17f52c05ef7872ee98ac9e81e432da3b65bd1722
salahbm <salahbm.001@gmail.com>
2024-02-27 20:18:59 +0900

    transformation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import Header from '@/components/shared/Header';
+import { transformationTypes } from '@/constant';
 
-const AddTransformationType = () => {
-  return <div>AddTransformationType</div>;
+import { getUserById } from '@/lib/actions/user.actions';
+import { SearchParamProps, TransformationTypeKey } from '@/types';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect('/sign-in');
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        {/* <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        /> */}
+      </section>
+    </>
+  );
 };
 
-export default AddTransformationType;
+export default AddTransformationTypePage;
